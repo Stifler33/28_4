60e8430ef48fd9db34c19e739ef44ef4d5ca8289
stifler33 <bagi33.ru@gmail.com>
2021-12-13 22:05:43 +0300

    task3 finish

--- task3.cpp
+++ task3.cpp
@@ -6,75 +6,86 @@
 #include "mainRandFunction.h"
 using namespace std;
 int countOrder = 0;
-vector<string> orderCareer;
-mutex orderCareer_access;
+int countDelivery = 0;
+mutex delivery_access;
+vector<string> orderCourier;
+mutex orderCourier_access;
 map<int, string> turn;
 mutex turn_access;
+void courier(){
+    this_thread::sleep_for(chrono::seconds(30));
+    while(!orderCourier_access.try_lock());
+    orderCourier.clear();
+    orderCourier_access.unlock();
+    while(!delivery_access.try_lock());
+    countDelivery++;
+    delivery_access.unlock();
+    cout << "delivery arrived " << countDelivery << endl;
+    if (countDelivery < 10) courier();
+}
 void kitchen(){
-        while (!turn_access.try_lock());
-        map<int, string>::iterator itTurn = turn.begin();
+    while (!turn_access.try_lock());
+    map<int, string>::iterator itTurn;
+    if(!turn.empty()){
+        itTurn = turn.begin();
+        turn_access.unlock();
+    }else {
+        turn_access.unlock();
+        this_thread::sleep_for(chrono::milliseconds(500));
+        kitchen();
+    }
 #define NUM_ORDER itTurn->first
 #define DISH itTurn->second
-        //turn[numberOrder] = dish;
-        cout << "kitchen: " << "accepted order " << NUM_ORDER << " " << DISH << endl;
-        timeSrand();
-        this_thread::sleep_for(chrono::seconds(randMinMax(5, 15)));
-        cout << "kitchen: " << "ready order " << NUM_ORDER << " " << DISH << endl;
-        if (orderCareer_access.try_lock()) {
-            cout << "kitchen: " << "order " << NUM_ORDER << " conveyed courier\n" << endl;
-            orderCareer.push_back(DISH);
-            turn.erase(NUM_ORDER);
-            orderCareer_access.unlock();
-            turn_access.unlock();
-        } else {
-            cout << "kitchen: " << "order " << NUM_ORDER << " wait courier\n";
-            while (!orderCareer_access.try_lock());
-            cout << "kitchen: " << "order " << NUM_ORDER << " conveyed courier\n" << endl;
-            orderCareer.push_back(DISH);
-            turn.erase(NUM_ORDER);
-            orderCareer_access.unlock();
-            turn_access.unlock();
-        }
+    //turn[numberOrder] = dish;
+    cout << "kitchen: " << "accepted order " << NUM_ORDER << " " << DISH << endl;
+    timeSrand();
+    this_thread::sleep_for(chrono::seconds(randMinMax(5, 15)));
+    cout << "kitchen: " << "ready order " << NUM_ORDER << " " << DISH << endl;
+    if (orderCourier_access.try_lock()) {
+        cout << "kitchen: " << "order " << NUM_ORDER << " conveyed courier\n" << endl;
+        orderCourier.push_back(DISH);
+        turn.erase(NUM_ORDER);
+        orderCourier_access.unlock();
+    } else {
+        cout << "kitchen: " << "order " << NUM_ORDER << " wait courier\n";
+        while (!orderCourier_access.try_lock());
+        cout << "kitchen: " << "order " << NUM_ORDER << " conveyed courier\n" << endl;
+        orderCourier.push_back(DISH);
+        turn.erase(NUM_ORDER);
+        orderCourier_access.unlock();
+    }
+    while(!delivery_access.try_lock());
+    if (countDelivery < 10) {
+        delivery_access.unlock();
+        kitchen();
+    }else delivery_access.unlock();
 }
 mutex kitchen_access;
-/*
+
 void order(){
-    timeSrand();
-    int numberOrder = countOrder;
-    string menu[] = {"pizza", "salad", "soup", "steak", "sushi"};
-    string dish = menu[randNum(5)];
-    cout << "!!!NEW order " << numberOrder << " : " << dish << endl;
-    while(!turn_access.try_lock());
-    turn[numberOrder] = dish;
-    turn_access.unlock();
-    if (kitchen_access.try_lock()){//если кухня свободна
-        cout << "order " << numberOrder << " : " << dish << " to kitchen\n";
-        kitchen(dish, numberOrder);//передаем заказ на кухню
-        kitchen_access.unlock();
-    }else{
-        cout << "order " << numberOrder << " wait\n";
-        while (!kitchen_access.try_lock());//ждем когда кухня освободиться
-        cout << "order " << numberOrder << " to kitchen\n";
-        kitchen(dish, numberOrder);//передаем заказ на кухню
-        kitchen_access.unlock();
-    }
-}
-*/
-int main(){
-    for (int i = 0; i < 10; i++){
+    while(!delivery_access.try_lock());
+    if (countDelivery < 10) {
+        delivery_access.unlock();
         timeSrand();
         this_thread::sleep_for(chrono::seconds(randMinMax(5, 10)));
         countOrder++;
         string menu[] = {"pizza", "salad", "soup", "steak", "sushi"};
         string dish = menu[randNum(5)];
         cout << "!!!NEW order " << countOrder << " : " << dish << endl;
-        while(!turn_access.try_lock());
+        while (!turn_access.try_lock());
         turn[countOrder] = dish;
-        thread th_kitchen(kitchen);
-        th_kitchen.detach();
         turn_access.unlock();
-    }
-    this_thread::sleep_for(chrono::seconds(100));
+        order();
+    }else delivery_access.unlock();
+}
+
+int main(){
+    thread th_Courier(courier);
+    th_Courier.detach();
+    thread th_Kitchen(kitchen);
+    th_Kitchen.detach();
+    order();
+    //this_thread::sleep_for(chrono::seconds(100));
     return 0;
 }
 
